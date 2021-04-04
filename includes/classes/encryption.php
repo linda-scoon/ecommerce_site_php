d0eb6ea768eb838c9b39e2f2d0c2e8609146421e
Linda <linda.scoon@hotmail.com>
2021-04-04 15:58:14 +0100

    add comments

--- includes/classes/encryption.php
+++ includes/classes/encryption.php
@@ -1,20 +1,37 @@
 <?php
+/**
+ * Encrypts and decrypts data
+ */
 class encryption
 {
     private $cipher;
     private $key;
-
+/**
+ * constructor
+ *
+ * @param string $key
+ * @param string $cipher
+ */
     function __construct($key, $cipher)
     {
         $this->cipher = $cipher;
         $this->key = $key;
     }
-
+/**
+ * key generator
+ *
+ * @return string encryption key
+ */
     function generate_key()
     {
         return openssl_random_pseudo_bytes(250);
     }
-
+/**
+ * encrytion function
+ *
+ * @param String $plaintext
+ * @return string $cipher
+ */
     function encrypt($plaintext)
     {
         $ivlen = openssl_cipher_iv_length($cipher = "AES-128-CBC");
@@ -23,7 +40,11 @@ class encryption
         $hmac = hash_hmac('sha256', $ciphertext_raw, $this->key, $as_binary = true);
         return  base64_encode($iv . $hmac . $ciphertext_raw);
     }
-
+/**
+ * decrytion function
+ *
+ * @return string $plaintext
+ */
     function decrypt()
     {
         $orig_text = '';
@@ -34,7 +55,7 @@ class encryption
         $ciphertext_raw = substr($c, $ivlen + $sha2len);
         $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $this->key, $options = OPENSSL_RAW_DATA, $iv);
         $calcmac = hash_hmac('sha256', $ciphertext_raw, $this->key, $as_binary = true);
-        if (hash_equals($hmac, $calcmac)) //PHP 5.6+ timing attack safe comparison
+        if (hash_equals($hmac, $calcmac)) 
         {
             $orig_text = $original_plaintext;
         }
